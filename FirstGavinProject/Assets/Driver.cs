25b4459d614c09118094ebff0ba598087a3d9ab2
Gavin <gavin.feeney01@gmail.com>
2022-01-01 21:45:04 +0000

    Add simple vars for movement control

--- FirstGavinProject/Assets/Driver.cs
+++ FirstGavinProject/Assets/Driver.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    // var declaration
+    float moveSpeed = 0.005f;
+    float turnSpeed = 0.1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +17,7 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, 0.1f);
-        transform.Translate(0, 0.005f, 0);
+        transform.Rotate(0, 0, turnSpeed);
+        transform.Translate(0, moveSpeed, 0);
     }
 }
