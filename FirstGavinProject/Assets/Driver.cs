7f30cd4da02b0af58daf2a82c373112d275b3462
Gavin <gavin.feeney01@gmail.com>
2022-01-11 20:19:43 +0000

    Add steer amount control

--- FirstGavinProject/Assets/Driver.cs
+++ FirstGavinProject/Assets/Driver.cs
@@ -17,7 +17,8 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, turnSpeed);
+        float steerAmount = Input.GetAxis("Horizontal");
+        transform.Rotate(0, 0, steerAmount);
         transform.Translate(0, moveSpeed, 0);
     }
 }
