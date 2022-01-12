75086a2348b34c82a020b48c643ff486177f91ab
Gavin <gavin.feeney01@gmail.com>
2022-01-12 20:39:29 +0000

    Add camera tracking to car position

--- FirstGavinProject/Assets/FollowCamera.cs
+++ FirstGavinProject/Assets/FollowCamera.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] GameObject carToFollow;
     void Update()
     {
-        
+        transform.position = carToFollow.transform.position + new Vector3(0, 0, -10);
     }
 }
