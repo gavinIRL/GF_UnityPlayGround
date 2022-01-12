8dc4edd73cbe9c820f70537d061935877a94232c
Gavin <gavin.feeney01@gmail.com>
2022-01-12 16:57:56 +0000

    Start driver facing correct direction

--- FirstGavinProject/Assets/Driver.cs
+++ FirstGavinProject/Assets/Driver.cs
@@ -11,7 +11,7 @@ public class Driver : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.Rotate(0, 0, -90);
+        transform.Rotate(0, 0, 90);
     }
 
     // Update is called once per frame
