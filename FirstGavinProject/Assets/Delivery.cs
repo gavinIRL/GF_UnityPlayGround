60a9ac28e7f76151b6f283fb6d190fe4b1fc084e
Gavin <gavin.feeney01@gmail.com>
2022-01-15 14:53:25 +0000

    Add colour change when target achieved

--- FirstGavinProject/Assets/Delivery.cs
+++ FirstGavinProject/Assets/Delivery.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class Delivery : MonoBehaviour
 {
     bool hasTargetOnboard = false;
+    [SerializeField] Color32 hasTargetColour = new Color32(20, 200, 40, 255);
+    [SerializeField] Color32 noTargetColour = new Color32(200, 20, 140, 255);
+    SpriteRenderer spriteRenderer;
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Object hit");
@@ -17,11 +24,13 @@ public class Delivery : MonoBehaviour
             Debug.Log("Reached the target");
             hasTargetOnboard = true;
             Destroy(other, 0.1f);
+            spriteRenderer.color = hasTargetColour;
         }
         if (other.tag == "Checkpoint" && hasTargetOnboard)
         {
             Debug.Log("Reached the destination");
             hasTargetOnboard = false;
+            spriteRenderer.color = noTargetColour;
         }
     }
 }
